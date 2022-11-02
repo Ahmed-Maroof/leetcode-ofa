d6d648065b386032c69db19cdbb32223e80d65d9
ahmedmaroof <ahmedmaroof2020@gmail.com>
2022-11-03 00:05:57 +0200

    roman_to_integer#2

--- roman_to_integer.rb
+++ roman_to_integer.rb
@@ -1,21 +1,23 @@
 # s = 'XXVII'
-s = 'MCMXCIV'
+s = 'LVIII'
 s_chars = s.split('')
-mapping = { 'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M': 1000 }
+s_chars_length = s_chars.length
+mapping = { 'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000 }
 output = 0
-
-s_chars.each_with_index do |char, i|
-  if mapping[char] == 1000
+i = 0
+while i < s_chars.length
+  if mapping[s_chars[i]] == 1000
     output += 1000
   else
-    if nil != mapping[s_chars[i + 1]] && mapping[s_chars[i + 1]] > mapping[char]
-      output += mapping[s_chars[i + 1]] - mapping[char]
+    if nil != mapping[s_chars[i + 1]] && mapping[s_chars[i + 1]] > mapping[s_chars[i]]
+      output += mapping[s_chars[i + 1]] - mapping[s_chars[i]]
       i += 1
     else
-      output += mapping[char]
+      output += mapping[s_chars[i]]
     end
 
   end
+  i += 1
 end
 puts output
 output
